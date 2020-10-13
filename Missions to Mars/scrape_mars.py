644b53f21c94ed45c1853a72a361f757deeec1b2
Adrian <azmodea@gmail.com>
2020-10-13 14:56:28 -0500

    Backing up progress

--- Missions to Mars/scrape_mars.py
+++ Missions to Mars/scrape_mars.py
@@ -8,33 +8,21 @@ def init_browser():
     executable_path = {"executable_path": "chromedriver"}
     return Browser("chrome", **executable_path, headless=False)
 
-
 def scrape():
-    browser = init_browser()
-    listings = []
-    listing = {}
+browser = init_browser()
 
-    url = "https://mars.nasa.gov/news/"
-    browser.visit(url)
-    time.sleep(1)
 
-    html = browser.html
-    soup = BeautifulSoup(html, "html.parser")
-    results = soup.find_all("li", class_="slides")
-    for r in results :
-	print(result("div", class_="content_title").text)
-        
-	try:
-            listing = {} # class ! This was the problem, I needed to reset the listing dictionary,  it was trying to insert the same item over and over
-            listing["headline"] = i.find("a", class_="result-title").get_text()
-            listing["price"] = i.find("span", class_="result-price").get_text()
-            listing["hood"] = i.find("span", class_="result-hood").get_text()
-            listing["img_url"] = i.find("div", class_="swipe").find("img")["src"]
-            listings.append(listing)
-        except:
-           pass
+url = "https://mars.nasa.gov/news/"
+browser.visit(url)
+time.sleep(2)
 
-    browser.quit()
+html = browser.html
+soup = BeautifulSoup(html, "html.parser")
+results = soup.find_all("div", class_="content_title")
+for r in results[1:41]:
+    print(r.text)
 
-    return listings
+
+
+
 
