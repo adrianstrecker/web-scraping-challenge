ea49a919eb1132cf444cbaf88237888ef1066539
Adrian <azmodea@gmail.com>
2020-10-15 15:50:06 -0500

    Backing up progress

--- Missions to Mars/app.py
+++ Missions to Mars/app.py
@@ -11,36 +11,34 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def n_title():
-    news_title = mongo.db.news_title.find()
+    mars_info = mongo.db.mars_info.find()
     # news_p = mongo.db.news_p.find_one()
-    return render_template("index.html", news_title=news_title)
+    return render_template("index.html", mars_info=mars_info)
 
-@app.route("/")
-def n_para():
-    news_p = mongo.db.news_p.find()
-    # news_p = mongo.db.news_p.find_one()
-    return render_template("index.html", news_p=news_p)
+# def n_para():
+#     news_p = mongo.db.news_p.find()
+#     # news_p = mongo.db.news_p.find_one()
+#     return render_template("index.html", news_p=news_p)
 
 
 @app.route("/scrape")
 def scraper():
-    news_title = mongo.db.news_title
-    news_title.drop()
-    news_t_data = scrape_mars.scrape()
+    mars_info = mongo.db.mars_info
+    mars_info.drop()
+    mars_data = scrape_mars.scrape()
+    # mongo.db.collection.update({}, mars_data, upsert=True)
+
+    mars_info.insert_many(mars_data)
+    return redirect("/", code=302)
+
+# def news_p_run():
+#     news_p = mongo.db.news_p
+#     news_p.drop()
+#     news_p_data = scrape_mars.scrape()
     # mongo.db.collection.update({}, news_t_data, upsert=True)
 
-    news_title.insert_many(news_t_data)
-    return redirect("/", code=302)
-    
-@app.route("/scrape")
-def news_p_run():
-    news_p = mongo.db.news_p
-    news_p.drop()
-    news_p_data = scrape_mars.scrape()
-    # mongo.db.collection.update({}, news_p_data, upsert=True)
-
-    news_p.insert_many(news_p_data)
-    return redirect("/", code=302)
+    # news_p.insert_many(news_p_data)
+    # return redirect("/", code=302)
 
 
 if __name__ == "__main__":
